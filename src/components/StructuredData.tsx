c4b29a40c040751beb620eff6634ba304c1340cc
carhubcentralts-hue <carhubcentralts@gmail.com>
2026-02-25 16:53:39 +0200

    Update LinkedIn URL in StructuredData component

--- src/components/StructuredData.tsx
+++ src/components/StructuredData.tsx
@@ -41,7 +41,7 @@ function getOrganizationSchema() {
       'https://wa.me/972557270844',
       'https://www.facebook.com/profile.php?id=61574820643163',
       'https://www.instagram.com/prosaas.ai/',
-      'https://www.linkedin.com/company/prosaas/',
+      'https://www.linkedin.com/company/prosaas-%D7%A4%D7%A8%D7%95%D7%A1%D7%90%D7%A1',
       'https://x.com/ProSaaS',
     ],
     contactPoint: {
